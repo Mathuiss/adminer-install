7fdc68487f75cbccb6aa816cf3ccc89e745456c3
Unknown <mathijsgeerlings@gmail.com>
2019-09-10 12:21:13 +0200

    QA config for adminer

---
+++ README.md
@@ -0,0 +1,13 @@
+# Adminer Bootstrap-Like Design
+
+[Adminer](https://www.adminer.org) interface inspired by the [Bootstrap Framework](https://getbootstrap.com/docs/3.3) with [Font-Awesome icons](http://fontawesome.io).
+
+## How to use
+
+Just download the whole package to a public directory on your server.
+
+Optionally you can configure [plugins](https://www.adminer.org/plugins) in the _index.php_ file.
+
+## Print Screen and Video
+
+[![Adminer Bootstrap-Like Design](https://i.imgur.com/Hu9ANYR.png)](https://www.youtube.com/watch?v=fMFCuaJphVk "Adminer Sidebar Toggle")
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Adminer Bootstrap-Like Design
+ *
+ * @author  Natan Felles, https://natanfelles.github.io <natanfelles@gmail.com>
+ * @link    https://github.com/natanfelles/adminer-bootstrap-like
+ * @link    https://www.adminer.org/plugins/#use
+ * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+ */
+
+//ini_set('display_errors', 0);
+
+function adminer_object()
+{
+	// Required to run any plugin
+	include_once './plugins/plugin.php';
+
+	// Plugins auto-loader
+	foreach (glob('plugins/*.php') as $filename)
+	{
+		include_once "./{$filename}";
+	}
+
+	// Specify enabled plugins here
+	$plugins = [
+
+		new AdminerDatabaseHide([
+			'mysql',
+			'information_schema',
+			'performance_schema',
+			'sys',
+		]),
+
+		new AdminerCollations([
+			'utf8_general_ci',
+			'utf8mb4_general_ci',
+			'latin1_swedish_ci',
+		]),
+
+		new AdminerEditForeign,
+		new AdminerEnumOption,
+
+		new AdminerStructComments,
+		new AdminerTableStructure,
+		new AdminerTableIndexesStructure,
+
+		new AdminerJsonPreview,
+
+		new AdminerDumpJson,
+		new AdminerDumpZip,
+
+		//new AdminerISPConfig('admin', 'admin', 'https://server.domain.tld', true),
+
+		new AdminerBootstrapLike(false),
+	];
+
+	return new AdminerPlugin($plugins);
+}
+
+// Include original Adminer or Adminer Editor
+include './adminer-4.7.0.php';
+// include './editor-4.7.0.php';
+?>
